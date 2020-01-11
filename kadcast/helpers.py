89cfcbb7237782e3c620bdd80905640864a47ea2
Jan Pressler <jan.pressler@mailbox.org>
2020-01-11 14:52:27 +0100

    Add missing file

---
+++ kadcast/helpers.py
@@ -0,0 +1,18 @@
+import simpy
+
+class Connection(object):
+    """This class represents the propagation through a cable."""
+    def __init__(self, env, delay):
+        self.env = env
+        self.delay = delay
+        self.store = simpy.Store(env)
+
+    def latency(self, value):
+        yield self.env.timeout(self.delay)
+        self.store.put(value)
+
+    def put(self, value):
+        self.env.process(self.latency(value))
+
+    def get(self):
+        return self.store.get()
